6c6b096411020786577f307cc5cfabf7e63b10c1
Linus Anderson <la222tc@student.lnu.se>
2014-11-10 10:49:40 +0000

    Done

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -9,17 +9,23 @@ var makePerson = function(persArr){
         
         return name.name;
         
-    }).sort().join(', ');
+    }).sort(function (a, b) {
+        
+        return a.localeCompare(b);
+    }).join(", ");
+    
     
     var ageArr = persArr.map(function(age) {
         
         return age.age;
     }).sort(function (a, b) {return a-b;});
     
-    var minAge = Math.min(ageArr);
-    var maxAge = Math.max(ageArr);
+    var minAge = Math.min.apply(Math, ageArr);
+    var maxAge = Math.max.apply(Math, ageArr);
+    
     var averageAge = ageArr.reduce(function (a, b) { return a+b;})
     averageAge = averageAge / ageArr.length;
+    averageAge = Math.ceil(averageAge);
     
     var result = {minAge: minAge, maxAge: maxAge, averageAge: averageAge, names: nameArr};
     
