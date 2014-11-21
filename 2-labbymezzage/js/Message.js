c5fd924c267b2277293055334f25ccc42e25b0f8
Linus Anderson <la222tc@student.lnu.se>
2014-11-21 20:33:46 +0000

    As good as done

--- 2-labbymezzage/js/Message.js
+++ 2-labbymezzage/js/Message.js
@@ -13,7 +13,7 @@ function Message(message, date) {
     };
     
     
-    this.getDate = function() {
+    this.getTime = function() {
     
     // Tar ut timmar, minuter och sekunder ur date och sätter ihop dom i en variabel och returnerar det    
    // var _date = new Date();
@@ -26,6 +26,10 @@ function Message(message, date) {
     return theDate;
     };
     
+    this.getDate = function(){
+        return date;
+    };
+    
     this.setDate = function(_date) {
         date = _date;
     };
@@ -35,14 +39,18 @@ function Message(message, date) {
     };
     
     Message.prototype.getHTMLText = function() {
-        return this.getText();
+       return this.getText().replace(/\n/g, "<br />");
     };
     
     Message.prototype.getDateText = function() {
-        var day = date.getDay();
-        var month = date.getMonth();
-        var year = date.getFullYear();
-        return day + month + year + this.getDate(); 
+        var monthNames = [ "Januari", "Februari", "Mars", "April", "Maj", "Juni",
+            "Juli", "Augusti", "September", "Oktober", "November", "December" ];
+            
+            var month = monthNames[date.getMonth()];
+            
+        return "Inlägget skapades den " + date.getDate() + " " + 
+		month + " " + date.getFullYear() + " " + " klockan " +
+		date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
     };
     
 }
--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -16,7 +16,23 @@ var MessageBoard = {
             displayMessages();
             numberOfMessages();
         });
-      
+        
+        var text = document.getElementById("messageTextArea");
+            
+        text.addEventListener("keypress", function (e) {
+                
+            if (e.keyCode === 13 && e.shiftKey) {
+                return "\r\n";
+            }
+            
+            else if (e.keyCode === 13) {
+                
+                createMessage();
+                numberOfMessages();
+                displayMessages();
+                text.value = "";
+            }
+            });
        
         function numberOfMessages(){
             var nr = document.getElementById('numberOfMessages');
@@ -44,7 +60,7 @@ var MessageBoard = {
             var timeStampP = document.createElement("p");
             var delTimeAndTime = document.createElement("div");
             
-            newDiv.id = "message" + messageID;
+            newDiv.className = "message";
             newP.className = "theMessage";
             aDelete.className = "timeAndDelete";
             aTime.className = "timeAndDelete";
@@ -58,16 +74,8 @@ var MessageBoard = {
             imgTime.setAttribute("src", "timestamppic.ico");
             aTime.setAttribute("href", "#");
             
-            newDiv.style.overflow = "hidden";
-            newDiv.style.backgroundColor = "#7897AB";
-            newDiv.style.textAlign = "left";
-            newDiv.style.width = "98%";
-            newDiv.style.cssFloat = "left";
-            newDiv.style.marginTop = "10px";
-            newDiv.style.marginLeft = "5px";
-            newDiv.style.borderRadius = "5px";
-            newDiv.style.border ="1px solid black";
-            
+            imgDelete.alt = "Delete";
+            imgTime.alt = "Fulltime";
    
             
             msg.appendChild(newDiv);
@@ -77,17 +85,40 @@ var MessageBoard = {
             newDiv.appendChild(imgDelete);
             newDiv.appendChild(imgTime);
             newDiv.appendChild(timeStampP);
+            newDiv.appendChild(delTimeAndTime);
             aDelete.appendChild(imgDelete);
             aTime.appendChild(imgTime);
             delTimeAndTime.appendChild(aDelete);
             delTimeAndTime.appendChild(aTime);
-            newDiv.appendChild(delTimeAndTime);
+            
             delTimeAndTime.appendChild(timeStampP);
             
             newP.innerHTML = MessageBoard.messages[messageID].getHTMLText();
-            timeStampP.innerHTML = MessageBoard.messages[messageID].getDate();
+            timeStampP.innerHTML = MessageBoard.messages[messageID].getTime();
             
-    
+            
+            imgDelete.onclick = function(){
+                removeMessage(messageID);
+            };
+            
+            imgTime.onclick = function () {
+            showTimeStamp(messageID);   
+        };
+        }   
+            
+        function removeMessage(messageID){
+            
+            var confirm = window.confirm("Vill du verkligen radera meddelandet?");
+            
+            if (confirm) {
+                MessageBoard.messages.splice(messageID, 1);
+                numberOfMessages();
+                displayMessages();
+            }
+            
+        }
+        function showTimeStamp(messageID) {
+            alert(MessageBoard.messages[messageID].getDateText());
         }
         
         function createMessage(){
@@ -95,12 +126,7 @@ var MessageBoard = {
             var mess = new Message(document.getElementById("messageTextArea").value, new Date());
             MessageBoard.messages.push(mess);
         }
-        
-        var del = document.getElementById("")
-        
-        function removeMessage(messageID){
-            
-        }
+
        
     }
     
