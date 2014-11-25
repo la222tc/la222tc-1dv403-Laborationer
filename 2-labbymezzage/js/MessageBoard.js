3bd451f9ec71cb46bd39e41dec38de38da91e5b6
Linus Anderson <la222tc@student.lnu.se>
2014-11-25 08:28:44 +0000

    Small change

--- 2-labbymezzage/js/MessageBoard.js
+++ 2-labbymezzage/js/MessageBoard.js
@@ -31,7 +31,6 @@ var MessageBoard = {
                 createMessage();
                 numberOfMessages();
                 displayMessages();
-                text.value = "";
             }
             });
        
